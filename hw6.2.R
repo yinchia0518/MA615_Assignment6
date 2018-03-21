661fd2222a2bc6fa9d932f9a662fb2f76764e1de
yinchia0518 <yinchia0518@gmail.com>
2018-03-21 17:31:37 -0400

    updated

--- hw6.2.R
+++ hw6.2.R
@@ -9,109 +9,111 @@ library(tidyverse)
 
 #Bude
 gc_Bude <- geocode("Bude, England, UK")
-map_Bude_road <- get_map(gc_Bude, maptype = "roadmap", zoom=15)
-map_Bude_water <- get_map(gc_Bude, maptype = "watercolor",zoom=15)
-
-bb_Bude <- attr(map_Bude, "bb")
-bbox_Bude <- bb2bbox(bb_Bude)
-
-data_Bude <- gc_Bude
+data_Bude <- as.data.frame(gc_Bude)
+map_Bude_road <- get_map(data_Bude, maptype = "roadmap", zoom=13)
+map_Bude_water <- get_map(data_Bude, maptype = "watercolor",zoom=13)
 
 #Bude_Road Map
-ggmap(map_Bude_road) +
+Bude_RoadMap <- ggmap(map_Bude_road) +
   geom_point(
     aes(x = lon, y = lat),
-    data = gc_Bude, color = "red", size = 3
+    data = data_Bude, color = "red", size = 3
   )
 
 #Bude_Water Map
-ggmap(map_Bude_water) +
+Bude_WaterMap <- ggmap(map_Bude_water) +
   geom_point(
     aes(x = lon, y = lat),
-    data = gc_Bude, color = "red", size = 3
+    data = data_Bude, color = "red", size = 3
   )
 
 
 #Cricket ground: Bude North Cornwall Cricket Club
 gc_NorthCornwall <- geocode("Bude North Cornwall Cricket Club")
-map_NorthCornwall <- get_map(gc_NorthCornwall)
+data_cricket <- as.data.frame(gc_NorthCornwall)
+map_NorthCornwall <- get_map(data_cricket)
 
-bb_NorthCornwall <- attr(map_NorthCornwall, "bb")
-bbox_NorthCornwall <- bb2bbox(bb_NorthCornwall)
-
-ggmap(map_NorthCornwall) +
+Cricket_RoadMap <- ggmap(map_NorthCornwall) +
   geom_point(
     aes(x = lon, y = lat),
-    data = gc_NorthCornwall, color = "yellow", size = 3
+    data = data_cricket, color = "yellow", size = 3
   )
-
-data_cricket <- gc_NorthCornwall
 
 #Route from Cricket ground to nearby pub 
 #Nearby Pub : Bar 35, Belle Vue Ln, Bude EX23 8BR, UK
+gc_Bar35 <- geocode("Bar 35, Belle Vue Ln, Bude EX23 8BR, UK")
+data_pub <- as.data.frame(gc_Bar35)
+
 from <- "Bar 35, Belle Vue Ln, Bude EX23 8BR, UK"
 to <- "Bude North Cornwall Cricket Club"
 route_df <- route(from, to, structure = "route")
 
-map_Bude_road <- get_map(gc_Bude, maptype = "roadmap", zoom=16)
-ggmap(map_Bude_road) +  
-  geom_path(
+route_data <- data.frame(x = route_df$lon, y= route_df$lat)
+
+map_Bude_road <- get_map(data_Bude, maptype = "roadmap", zoom=16)
+
+Route_CricketBar <- ggmap(map_Bude_road) +  
+    geom_path(
     aes(x = lon, y = lat), colour = "red", size = 1.5,
     data = route_df, lineend = "round"
   )
 
-gc_Bar35 <- geocode("16 Erdiston Ct, Bude EX23, UK")
-data_pub <- gc_Bar35
-
 #Beach1: Sandy Mouth Beach
-gc_SandyMouth <- geocode("S W Coast Path, Bude EX23, UK")
-map_SandyMouth <- get_map(gc_SandyMouth, zoom=16)
+gc_SandyMouth <- geocode("Kilkhampton, Bude EX23 9EG, UK")
+data_beach1 <- as.data.frame(gc_SandyMouth)
+map_SandyMouth <- get_map(data_beach1, zoom=16)
 
-bb_SandyMouth <- attr(map_SandyMouth, "bb")
-bbox_SandyMouth <- bb2bbox(bb_SandyMouth)
-
-ggmap(map_SandyMouth) +
+SandyMouth_RoadMap <- ggmap(map_SandyMouth) +
   geom_point(
     aes(x = lon, y = lat),
-    data = gc_SandyMouth, color = "green", size = 4
+    data = data_beach1, color = "green", size = 4
   )
-
-data_beach1<-gc_SandyMouth
 
 #Beach2:Summerleaze Beach
 
 gc_Summerleaze <- geocode("Summerleaze Cres, Bude EX23 8HN, UK")
-map_Summerleaze <- get_map(gc_Summerleaze)
+data_beach2 <- as.data.frame(gc_Summerleaze)
+map_Summerleaze <- get_map(data_beach2)
 
-bb_Summerleaze <- attr(map_Summerleaze, "bb")
-bbox_Summerleaze <- bb2bbox(bb_Summerleaze)
-
-ggmap(map_Summerleaze) +
+Summerleaze_RoadMap <- ggmap(map_Summerleaze) +
   geom_point(
     aes(x = lon, y = lat),
-    data = gc_Summerleaze, color = "green", size = 3
+    data = data_beach2, color = "green", size = 3
   )
-
-data_beach2 <- gc_Summerleaze
 
 
 #create a data frame containing all locations
-lon_all <- as.numeric(c( data_cricket[1], data_beach1[1],data_beach2[1],data_pub[1]))
-lat_all <- as.numeric(c( data_cricket[2],data_beach1[2],data_beach2[2], data_pub[2]))
+lon_all <- as.numeric(c(data_Bude[1], data_cricket[1], data_beach1[1],data_beach2[1],data_pub[1]))
+lat_all <- as.numeric(c(data_Bude[2], data_cricket[2],data_beach1[2],data_beach2[2], data_pub[2]))
 allData <- data.frame(lon=lon_all, lat=lat_all)
 
-# 
-# gc_England <- geocode("England, UK")
-# map_England <- get_map(gc_Bude, maptype = "roadmap", zoom=15)
 
-ggmap(map_Bude_road) +
+all_spots_Roadmap <- ggmap(map_Bude_road) +
+  geom_point(aes(x = lon, y = lat), data = data_Bude, color = "Brown", size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_cricket, color = "Red", size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_beach1, color = "yellow" , size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_beach2,color = "Green" , size = 3)+
-  geom_point(aes(x = lon, y = lat), data = data_pub, color = "Blue" , size = 3)
+  geom_point(aes(x = lon, y = lat), data = data_pub, color = "Blue" , size = 3)+
+  geom_text(aes(x = lon, y = lat, label="Bude"), data = data_Bude, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Cricket Club"), data_cricket, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Sandy Mouth Beach"), data = data_beach1, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Summerleaze Beach"), data = data_beach2, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Bar 35"), data = data_pub, vjust=0, hjust=0,size=3)+
+  geom_path(aes(x = lon, y = lat), colour = "red", size = 1,data = route_df, lineend = "round")
+
  
-ggmap(map_Bude_water) +
+all_spots_Watermap <- ggmap(map_Bude_water) +
+  geom_point(aes(x = lon, y = lat), data = data_Bude, color = "Brown", size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_cricket, color = "Red", size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_beach1, color = "yellow" , size = 3)+
   geom_point(aes(x = lon, y = lat), data = data_beach2,color = "Green" , size = 3)+
-  geom_point(aes(x = lon, y = lat), data = data_pub, color = "Blue" , size = 3)
+  geom_point(aes(x = lon, y = lat), data = data_pub, color = "Blue" , size = 3)+
+  geom_text(aes(x = lon, y = lat, label="Bude"), data = data_Bude, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Cricket Club"), data_cricket, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Sandy Mouth Beach"), data = data_beach1, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Summerleaze Beach"), data = data_beach2, vjust=1, hjust=1,size=3)+
+  geom_text(aes(x = lon, y = lat, label="Bar 35"), data = data_pub, vjust=0, hjust=0,size=3)+
+  geom_path(aes(x = lon, y = lat), colour = "red", size = 1,data = route_df, lineend = "round")
+
+
+
